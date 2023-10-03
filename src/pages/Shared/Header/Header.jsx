80cb6e3f773b0312a17f8fefbe385c6f83ef3c1b
Fahmida Faiza <fahmidafaiza57@gmail.com>
2023-10-03 15:36:50 +0600

    added time dynamic of moment

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,13 @@
-
+import logo from '../../../assets/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
         <div>
-            <h1 className="text-3xl">Header there</h1>
+            {/* <h1 className="text-3xl">Header there</h1> */}
+<img src={logo} alt="" />
+            <p>Journalism Without Fear or Favour</p>
+            <p className='text-xl'>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
     );
 };
